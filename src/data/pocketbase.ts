77df1c8f5cb62dcac4b271a3a529394a57539662
Flavio Copes <flavio@flaviocopes.com>
2024-03-06 12:20:18 +0100

    mod4

--- src/data/pocketbase.ts
+++ src/data/pocketbase.ts
@@ -1,10 +1,22 @@
-export async function getProjects(pb: any) {
-  const projects = await pb.collection('projects').getFullList()
+import type {
+  ProjectsRecord,
+  ProjectsResponse,
+  TasksRecord,
+  TasksResponse,
+  TypedPocketBase,
+} from '@src/data/pocketbase-types'
 
-  return projects
+type TexpandProject = {
+  project?: ProjectsResponse
 }
 
-export async function addProject(pb: any, name: string) {
+export async function getProjects(pb: TypedPocketBase) {
+  const projects = await pb.collection('projects').getFullList()
+
+  return projects.sort((a, b) => getStatus(a) - getStatus(b))
+}
+
+export async function addProject(pb: TypedPocketBase, name: string) {
   const newProject = await pb.collection('projects').create({
     name,
     status: 'not started',
@@ -13,13 +25,17 @@ export async function addProject(pb: any, name: string) {
   return newProject
 }
 
-export async function getProject(pb: any, id: string) {
+export async function getProject(pb: TypedPocketBase, id: string) {
   const project = await pb.collection('projects').getOne(id)
 
   return project
 }
 
-export async function addTask(pb: any, project_id: string, text: string) {
+export async function addTask(
+  pb: TypedPocketBase,
+  project_id: string,
+  text: string
+) {
   const newTask = await pb.collection('tasks').create({
     project: project_id,
     text,
@@ -28,12 +44,86 @@ export async function addTask(pb: any, project_id: string, text: string) {
   return newTask
 }
 
-export async function getTasks(pb: any, project_id: string) {
+export async function getTasks({
+  pb,
+  project_id = null,
+  done = false,
+}: {
+  pb: TypedPocketBase
+  project_id: string | null
+  done: boolean
+}): Promise<TasksResponse<TexpandProject>[]> {
   const options = {
-    filter: `project = "${project_id}"`,
+    filter: '',
+  }
+  let filter = `completed = ${done}`
+  filter += ` && project = "${project_id}"`
+  options.filter = filter
+
+  let tasks: TasksResponse<TexpandProject>[] = []
+
+  tasks = await pb.collection('tasks').getFullList(options)
+
+  return tasks
+}
+
+function getStatus(project: ProjectsResponse) {
+  switch (project.status) {
+    case 'not started':
+      return 7
+    case 'on hold':
+      return 6
+    case 'started':
+      return 5
+    case 'in progress':
+      return 4
+    case 'almost finished':
+      return 3
+    case 'ongoing':
+      return 2
+    case 'done':
+      return 1
+    default:
+      return 0
+  }
+}
+
+export async function deleteProject(pb: TypedPocketBase, id: string) {
+  await pb.collection('projects').delete(id)
+}
+
+export async function updateProject(
+  pb: TypedPocketBase,
+  id: string,
+  data: ProjectsRecord
+) {
+  await pb.collection('projects').update(id, data)
+}
+
+export async function deleteTask(pb: TypedPocketBase, id: string) {
+  await pb.collection('tasks').delete(id)
+}
+
+export async function updateTask(
+  pb: TypedPocketBase,
+  id: string,
+  data: TasksRecord
+) {
+  await pb.collection('tasks').update(id, data)
+}
+
+export async function getStarredTasks(
+  pb: TypedPocketBase
+): Promise<TasksResponse<TexpandProject>[]> {
+  const options = {
+    sort: '-starred_on',
+    filter: 'starred = true && completed = false',
+    expand: 'project',
   }
 
-  const tasks = await pb.collection('tasks').getFullList(options)
+  const tasks: TasksResponse<TexpandProject>[] = await pb
+    .collection('tasks')
+    .getFullList(options)
 
   return tasks
 }

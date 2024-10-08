0cf3f53743470a9606c5dc8f479492598335bd36
Flavio Copes <flavio@flaviocopes.com>
2024-10-08 16:48:33 +0200

    Init pocketbase in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,15 @@
 import { defineMiddleware } from 'astro/middleware'
 import PocketBase from 'pocketbase'
 
+import type { TypedPocketBase } from '@src/data/pocketbase-types'
+
 export const onRequest = defineMiddleware(async (context, next) => {
-  context.locals.pb = new PocketBase(process.env.POCKETBASE_URL)
+  context.locals.pb = new PocketBase(
+    process.env.POCKETBASE_URL
+  ) as TypedPocketBase
+
+  // globally disable auto cancellation
+  context.locals.pb.autoCancellation(false)
 
   return next()
 })

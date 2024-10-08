7055b473a9f1127dd34172be7900b232bdbb9b72
Flavio Copes <flavio@flaviocopes.com>
2024-10-08 16:10:31 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,5 @@ import PocketBase from 'pocketbase'
 export const onRequest = defineMiddleware(async (context, next) => {
   context.locals.pb = new PocketBase(process.env.POCKETBASE_URL)
 
-  // globally disable auto cancellation
-  context.locals.pb.autoCancellation(false)
-
   return next()
 })
